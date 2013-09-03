d6643e7ccf73151898df3dea877d4d1e281795d9
byrnefio <byrnefio@grinnell.edu>
2013-09-03 13:42:30 -0500

    Update HelloWorld.java

--- src/edu/grinnell/csc207/byrnefio/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/byrnefio/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package edu.grinnell.csc207.byrnefio.hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-	System.out.println("Hello, GitHub!");
+	System.out.println("Hello, GitHub! It's good to be here.");
     } // main(String[])
 }
